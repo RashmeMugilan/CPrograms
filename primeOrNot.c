9430c5695e4a305be90cdd702156f34b25c5b7e8
RashmeMugilan <124969950+RashmeMugilan@users.noreply.github.com>
2025-06-13 13:26:47 +0530

    Update primeOrNot.c

--- primeOrNot.c
+++ primeOrNot.c
@@ -1,6 +1,12 @@
+/* A prime number is a natural number greater than 1 that has only two distinct positive divisors: 1 and itself. 
+   In other words, a prime number cannot be divided evenly by any other number except 1 and the number itself. 
+   For example, 2, 3, 5, 7, 11, and 13 are all prime numbers. 
+   The number 1 is not considered a prime number because it only has one divisor (itself), not two distinct divisors.
+*/
+
 #include <stdio.h>
-#include <stdbool.h>
-#include <math.h>
+#include <stdbool.h> //header to use true or false
+#include <math.h> // to use mathematical calculations such as sqrt
 
 bool isPrime(int num) {
     if (num <= 1) {
