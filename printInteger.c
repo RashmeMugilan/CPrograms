bd1971658cb7b9a50e3b208482ae75ea26d2d79e
RashmeMugilan <124969950+RashmeMugilan@users.noreply.github.com>
2025-06-13 13:19:18 +0530

    C Program to Print an Integer Entered By the User

---
+++ printInteger.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+  int number;
+
+  printf("Enter an integer: ");
+  scanf("%d", &number);
+
+  printf("You entered: %d\n", number);
+
+  return 0;
+}
+
+/* Output: 
+Enter an integer: 5
+You entered: 5
+*/
